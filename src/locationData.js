c4360802de43e3acb924898a34157278c5dc1d99
irisalukiferriswheel <74806732+irisalukiferriswheel@users.noreply.github.com>
2026-08-08 17:27:15 -0400

    Add country and region data for onboarding

---
+++ src/locationData.js
@@ -0,0 +1,12 @@
+export const COUNTRIES = [
+  ['CA','Canada'],['US','États-Unis'],['FR','France'],['GB','Royaume-Uni'],['DE','Allemagne'],['IT','Italie'],['ES','Espagne'],['PT','Portugal'],['BE','Belgique'],['CH','Suisse'],['NL','Pays-Bas'],['IE','Irlande'],['AU','Australie'],['NZ','Nouvelle-Zélande'],['JP','Japon'],['KR','Corée du Sud'],['CN','Chine'],['IN','Inde'],['BR','Brésil'],['MX','Mexique'],['AR','Argentine'],['CL','Chili'],['CO','Colombie'],['PE','Pérou'],['ZA','Afrique du Sud'],['MA','Maroc'],['DZ','Algérie'],['TN','Tunisie'],['SN','Sénégal'],['CI','Côte d’Ivoire'],['CM','Cameroun']
+]
+
+export const REGIONS = {
+  CA: [
+    ['AB','Alberta'],['BC','Colombie-Britannique'],['MB','Manitoba'],['NB','Nouveau-Brunswick'],['NL','Terre-Neuve-et-Labrador'],['NS','Nouvelle-Écosse'],['NT','Territoires du Nord-Ouest'],['NU','Nunavut'],['ON','Ontario'],['PE','Île-du-Prince-Édouard'],['QC','Québec'],['SK','Saskatchewan'],['YT','Yukon']
+  ],
+  US: [
+    ['AL','Alabama'],['AK','Alaska'],['AZ','Arizona'],['AR','Arkansas'],['CA','California'],['CO','Colorado'],['CT','Connecticut'],['DE','Delaware'],['FL','Florida'],['GA','Georgia'],['HI','Hawaii'],['ID','Idaho'],['IL','Illinois'],['IN','Indiana'],['IA','Iowa'],['KS','Kansas'],['KY','Kentucky'],['LA','Louisiana'],['ME','Maine'],['MD','Maryland'],['MA','Massachusetts'],['MI','Michigan'],['MN','Minnesota'],['MS','Mississippi'],['MO','Missouri'],['MT','Montana'],['NE','Nebraska'],['NV','Nevada'],['NH','New Hampshire'],['NJ','New Jersey'],['NM','New Mexico'],['NY','New York'],['NC','North Carolina'],['ND','North Dakota'],['OH','Ohio'],['OK','Oklahoma'],['OR','Oregon'],['PA','Pennsylvania'],['RI','Rhode Island'],['SC','South Carolina'],['SD','South Dakota'],['TN','Tennessee'],['TX','Texas'],['UT','Utah'],['VT','Vermont'],['VA','Virginia'],['WA','Washington'],['WV','West Virginia'],['WI','Wisconsin'],['WY','Wyoming'],['DC','District of Columbia']
+  ]
+}
